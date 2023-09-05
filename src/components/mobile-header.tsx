c048ea994bb8e1d3a48fad8bb91664a23ac82930
Isaac Flores Carbajal <isaacflores590@gmail.com>
2023-09-05 13:57:32 -0500

    v.1.6.1

--- src/components/mobile-header.tsx
+++ src/components/mobile-header.tsx
@@ -2,13 +2,16 @@ import bar from '@/img/burger_1.svg';
 import Image from 'next/image';
 import LightSwitch from './light-switch';
 import Reveal from './reveal';
+import Link from 'next/link';
 
 export default function MobileHeader() {
 
   return (
     <header className='anima z-[2] flex flex-row items-center justify-center xsm:w-full  xsm:space-x-32 py-4'>
       <Reveal>
-        <h1 className='relative xsm:text-4xl w-fit h-fit font-semibold'>if(C)</h1>
+        <Link href="#homeSection">
+          <h1 className='relative xsm:text-4xl w-fit h-fit font-semibold'>if(C)</h1>
+        </Link>
       </Reveal>
       <Reveal>
         <div className='flex flex-row items-center space-x-4'>
