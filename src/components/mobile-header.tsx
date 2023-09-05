eeb52771e6c3725a5a5b2e18331816d754375c55
Isaac Flores Carbajal <isaacflores590@gmail.com>
2023-09-05 12:18:26 -0500

    v1.5

--- src/components/mobile-header.tsx
+++ src/components/mobile-header.tsx
@@ -6,7 +6,7 @@ import Reveal from './reveal';
 export default function MobileHeader() {
 
   return (
-    <header className='anima z-[2] flex flex-row items-center xsm:w-full justify-between py-4'>
+    <header className='anima z-[2] flex flex-row items-center xsm:w-full justify-center space-x-20 py-4'>
       <Reveal>
         <h1 className='relative xsm:text-4xl w-fit h-fit font-semibold'>if(C)</h1>
       </Reveal>
